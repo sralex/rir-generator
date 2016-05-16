f501fda4849f1d780d41aa1626a45c1e5d4419cf
Marvin Ritter <marvin.ritter@gmail.com>
2016-05-16 12:12:10 +0200

    Add Cython module around C++ library

---
+++ python/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import os.path
+import numpy as np
+
+# python3 setup.py build_ext --inplace
+
+extensions = [
+	Extension("*", ["pyrirgen.pyx"],
+		include_dirs=[
+			os.path.abspath("."),
+		],
+		library_dirs=[os.path.abspath(".")],
+		libraries=["rirgen"],
+		language='c++'
+	),
+]
+
+setup(
+	name='pyrirgen',
+	ext_modules=cythonize(extensions, compiler_directives = {
+		'language_level': 3, # Python 3
+		'embedsignature': True, # add method signature to docstrings, thus tools can display it after compilation
+		'c_string_encoding': 'default',
+		'c_string_type': 'str',
+	}),
+)
